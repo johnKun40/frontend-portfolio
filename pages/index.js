3aa68048dec54813e0abf37715dcb4ab9c0645b3
johnKun40 <johnkun40@gmail.com>
2022-08-26 18:21:10 +0100

    fixing bugs

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import Navbar from '../components/Navbar';
 import Projects from '../components/Projects';
 import Skills from '../components/Skills';
 import HashLoader from "react-spinners/HashLoader";
-import Slideshow from '../components/Slideshow';
+
 
 
 
