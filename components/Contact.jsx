967b0248e26916ac2f643920f4097d5d828b0204
johnKun40 <johnkun40@gmail.com>
2022-08-29 14:19:03 +0100

    Update toast

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -62,7 +62,7 @@ function Contact() {
 
   return (
     <div id='contact' className='w-full lg:h-screen'>
-        <Toaster position="bottom-center" reverseOrder={false} />
+        <Toaster position="top-center" reverseOrder={false} />
         <div className='max-w-[1240px] m-auto px-2 py-16 w-full'>
             <p className='text-xl tracking-widest uppercase'>Contact</p>
             <h2 className='py-4'>Get In Touch</h2>
